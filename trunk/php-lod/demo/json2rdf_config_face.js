d7dea146f3c6ae5815d4dfee0a5f47caa8ad26d5
lidingpku@gmail.com <lidingpku@gmail.com@08aa6e76-ee46-72b6-37af-ed63ab528467>
2011-01-06 16:25:27 +0000

    new configuration file, now support generating multiple triples for the same predicate

---
+++ trunk/php-lod/demo/json2rdf_config_face.js
@@ -0,0 +1,22 @@
+{
+"ns_property":"http://lod-apps.googlecode.com/files/face.rdf#",
+"propmap":{
+  "picture":["http://opengraphprotocol.org/schema/image","http://xmlns.com/foaf/0.1/depiction"],
+  "name":["http://opengraphprotocol.org/schema/title","http://xmlns.com/foaf/0.1/name"],
+  "link":["http://opengraphprotocol.org/schema/url","http://xmlns.com/foaf/0.1/homepage"]},
+  "category":["http://opengraphprotocol.org/schema/type","http://purl.org/dc/terms/type"]},
+
+  "first_name":["http://xmlns.com/foaf/0.1/firstName"],
+  "last_name":["http://xmlns.com/foaf/0.1/lastName"],
+  "from":["http://purl.org/dc/terms/source"],
+  "id":["http://purl.org/dc/terms/identifier"],
+  "created_time":["http://purl.org/dc/terms/created"],
+  "updated_time":["http://purl.org/dc/terms/modified"]
+
+},
+"namespacemap":[
+  {"prefix":"foaf","ns":"http://xmlns.com/foaf/0.1/"},
+  {"prefix":"dcterms","ns":"http://purl.org/dc/terms/"},
+  {"prefix":"og","ns":"http://opengraphprotocol.org/schema/"}
+]
+}
