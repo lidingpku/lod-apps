09e52057f329814281498304bd28f7611a6d682a
lidingpku@gmail.com <lidingpku@gmail.com@08aa6e76-ee46-72b6-37af-ed63ab528467>
2011-01-06 16:35:18 +0000

    fix json bug

--- php-lod/demo/json2rdf_config_face.js
+++ php-lod/demo/json2rdf_config_face.js
@@ -3,8 +3,8 @@
 "propmap":{
   "picture":["http://opengraphprotocol.org/schema/image","http://xmlns.com/foaf/0.1/depiction"],
   "name":["http://opengraphprotocol.org/schema/title","http://xmlns.com/foaf/0.1/name"],
-  "link":["http://opengraphprotocol.org/schema/url","http://xmlns.com/foaf/0.1/homepage"]},
-  "category":["http://opengraphprotocol.org/schema/type","http://purl.org/dc/terms/type"]},
+  "link":["http://opengraphprotocol.org/schema/url","http://xmlns.com/foaf/0.1/homepage"],
+  "category":["http://opengraphprotocol.org/schema/type","http://purl.org/dc/terms/type"],
 
   "first_name":["http://xmlns.com/foaf/0.1/firstName"],
   "last_name":["http://xmlns.com/foaf/0.1/lastName"],
